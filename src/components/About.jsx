ad5a1c4034e7692b3471dc22fcd6ab0ed7ba5a22
Habibur Rahman <habibthealien@gmail.com>
2023-09-14 20:04:43 +0600

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
 			className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8">
 			<SectionTitle title="About me" titleNo="01" />
 			<div className="flex flex-col lgl:flex-row gap-16">
-				<div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4 ">
+				<div className="w-full  lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4 ">
 					<p>
 						Hello, I'm Habibur Rahman, a dedicated web developer
 						with over 3 years of experience specializing in the{" "}
@@ -56,19 +56,19 @@ const About = () => {
 						))}
 					</ul>
 				</div>
-				<div className="w-full lgl:w-1/3 h-80 relative group">
-					<div className="absolute w-full h-80 -left-6 -top-6 rounded-lg">
-						<div className="w-full h-full relative z-20 flex lgl:pl-0">
+				<div className="w-5/6 self-center  lg:w-full  lgl:w-1/3 h-80 lg:self-start relative group">
+					<div className="absolute w-full h-80 -left-4 -top-4 rounded-lg">
+						<div className="w-full h-full relative z-20 flex  group-hover:-translate-x-2 group-hover:-translate-y transition-transform duration-300 ">
 							<Image
 								fill
 								src={Personal.image}
 								alt=""
-								className="rounded-lg ml-8 h-full md:ml-0  object-cover"
+								className="rounded-lg  h-full object-cover"
 							/>
-							<div className="hidden lgl:inline-block absolute w-full h-80 bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300"></div>
+							<div className="absolute w-full h-80 bg-textGreen/30 rounded-md top-0 left-0 group-hover:bg-transparent duration-300"></div>
 						</div>
 					</div>
-					<div className="hidden lgl:inline-flex w-full h-80 border-2 border-textGreen rounded-md group-hover:translate-x-2 group-hover:translate-y-2 transition-transform duration-300 "></div>
+					<div className=" w-full h-80 border-2 border-textGreen rounded-md group-hover:translate-x-2 group-hover:translate-y-2 transition-transform duration-300 "></div>
 				</div>
 			</div>
 		</section>
